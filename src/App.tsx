ab7e0f7fa8ba64a3c8048bf3ddbccc68a232ed92
sxcheng <1032211967@qq.com>
2018-07-30 13:37:46 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+// import { Router, Route, Switch } from 'react-router-dom';
+// const { }
+class App extends React.Component {
+  public render() {
+    return (
+      // <Router>
+      //   <Switch>
+
+      //   </Switch>
+      // </Router>
+      <div></div>
+    );
+  }
+}
+
+export default App;
---
+++ types/declare.d.ts
@@ -0,0 +1,17 @@
+import { ReactChildren } from "react";
+
+//使图片css可导入
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.less' {
+  const content: {[className: string]: string};
+  export = content;
+}
+
+
+interface PageData {
+  path: string;
+  component: ReactChildren;
+  name: string;
+}
